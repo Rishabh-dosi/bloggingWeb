11b8bf04ea928a064eb08f0135236981f47d8f84
Rishabh Dosi <rishabh3dosi@gmail.com>
2025-01-06 11:31:22 +0530

    jwt secret invalid error (typescript)

--- src/app/api/auth/verifyToken/route.ts
+++ src/app/api/auth/verifyToken/route.ts
@@ -6,7 +6,11 @@ export async function POST(req: Request) {
     try {
         const { token } = await req.json()
         if (token) {
-            const userData = await jwt.verify(token, process.env.JWT_SECRET);
+            const jwtSecret = process.env.JWT_SECRET;
+            if (!jwtSecret) {
+                return NextResponse.json({ message: "JWT secret is not defined" }, { status: 500 });
+            }
+            const userData = await jwt.verify(token, jwtSecret);
             return NextResponse.json({ userData });
         }
         return NextResponse.json({message: "Please Login"})
